ca5d6464824aaa78950c400e3fbfd5e8f188dba9
Mohit Raj <mohitraj3697@gmail.com>
2026-02-15 23:25:26 +0530

    added prompt

---
+++ prompt/chatbot.py
@@ -0,0 +1,28 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="mistralai/Mistral-7B-Instruct-v0.2",
+    task="text-generation",
+    max_new_tokens=50,
+)
+
+model = ChatHuggingFace(llm=llm)
+
+chat_history = [
+    SystemMessage(content='You are a helpful AI assistant')
+]
+
+while True:
+    user_input = input('You: ')
+    chat_history.append(HumanMessage(content=user_input))
+    if user_input == 'exit':
+        break
+    result = model.invoke(chat_history)
+    chat_history.append(AIMessage(content=result.content))
+    print("AI: ",result.content)
+
+print(chat_history)
