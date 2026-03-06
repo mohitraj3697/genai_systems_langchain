4927c65c1ed284bf560a81e15038faf0417c0a25
Mohit Raj <mohitraj3697@gmail.com>
2026-03-06 21:09:58 +0530

    added web csv loader

---
+++ loader/web_loader.py
@@ -0,0 +1,31 @@
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="mistralai/Mistral-7B-Instruct-v0.2",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+prompt = PromptTemplate(
+    template='Answer the following question \n {question} from the following text - \n {text}',
+    input_variables=['question','text']
+)
+
+parser = StrOutputParser()
+
+url = 'https://www.mohitrajdev.in/blog-details.html?id=nanochat-analysis'
+loader = WebBaseLoader(url)
+
+docs = loader.load()
+
+
+chain = prompt | model | parser
+
+print(chain.invoke({'question':'what is this blog about?', 'text':docs[0].page_content}))
